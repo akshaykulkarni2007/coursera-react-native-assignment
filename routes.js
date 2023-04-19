12f10d8b41fc3349aea0f58fb2df58a7f8b12add
Akshay Kulkarni <akshaykulkarni2007@gmail.com>
2023-04-19 22:39:32 +0530

    C05W04E03: TabNavigation

--- routes.js
+++ routes.js
@@ -1,18 +1,37 @@
 import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+// import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Ionicons from '@expo/vector-icons/Ionicons'
 
 import WelcomeScreen from './screens/WelcomeScreen'
 import LoginScreen from './screens/LoginScreen'
 
-const Stack = createNativeStackNavigator()
+// const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
 
 export default function Routes() {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator initialRouteName="Login">
+			{/* <Stack.Navigator initialRouteName="Login">
 				<Stack.Screen name="Welcome" component={WelcomeScreen} />
 				<Stack.Screen name="Login" component={LoginScreen} />
-			</Stack.Navigator>
+			</Stack.Navigator> */}
+			<Tab.Navigator
+				screenOptions={({ route }) => ({
+					tabBarIcon: ({ size }) => {
+						let iconName
+						if (route.name === 'Welcome') {
+							iconName = 'ios-home'
+						} else if (route.name === 'Login') {
+							iconName = 'ios-enter'
+						}
+						return <Ionicons name={iconName} size={size} />
+					},
+				})}
+				initialRouteName="Login">
+				<Tab.Screen name="Welcome" component={WelcomeScreen} />
+				<Tab.Screen name="Login" component={LoginScreen} />
+			</Tab.Navigator>
 		</NavigationContainer>
 	)
 }
